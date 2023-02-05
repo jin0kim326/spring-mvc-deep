6a2944fa8c9b396c0c7255fa915d1743d3534223
김진영C <jin0kim326@douzone.com>
2023-02-06 08:42:28 +0900

    [1.로그인 처리 : 쿠키, 세션] #6 로그인 처리 추가 : 서버(세션저장소) 기능 구현

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -14,6 +14,19 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+/**
+ * 현재 쿠키의 문제점
+ * 1. 쿠키값을 변조가능 : memberId:1 , memberId:2 => 해커가 특정멤버로 로그인하기 너무 쉬운구조
+ * 2. 쿠키에 보관하는 정보는 클라이언트 해킹 가능 : 주민번호,신용카드정보 등
+ * 3. 해커 쿠키 탈취 후 사용
+ *
+ * 해결방안 : 서버세션을 사용
+ * 1. 쿠키값 변조 => 세션아이디를 랜덤값으로 줌으로써 예측불가
+ * 2. 쿠키 정보 해킹 => 아무 의미없는 정보만 저장 (민감정보저장x)
+ * 3. 해커 쿠키 탈취 후 사용 => 일정시간(ex.30분) 후 만료
+ *
+ *
+ */
 @Slf4j
 @Controller
 @RequiredArgsConstructor
