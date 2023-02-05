933a70922fffe6ce8e00d5d53794dce2ba197781
김진영C <jin0kim326@douzone.com>
2023-02-05 18:55:41 +0900

    [로그인 처리1 : 쿠키, 세션] 홈 화면 구성 (패키지 구성)

--- login/src/main/java/hello/login/ItemServiceApplication.java
+++ login/src/main/java/hello/login/ItemServiceApplication.java
@@ -4,7 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * 패키지 구조
+ * domain / web
+ *
+ * 도메인 : 화면,ui,기술 인프라 등등의 영역은 제외한 시스템이 구현해야 하는 핵심 비즈니스 업무영역
+ * web을 다른 기술로 바꾸어도 도메인은 그대로 유지 !!
+ *
+ * =>
+ * < web은 도메인을 알고있지만, domain은 web을 모르도록 설계 >
+ * */
 @Slf4j
 @SpringBootApplication
 public class ItemServiceApplication {
