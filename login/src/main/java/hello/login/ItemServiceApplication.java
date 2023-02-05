bba4d470ca3c8fceb5343af24e62fa95ce37a5e9
김진영C <jin0kim326@douzone.com>
2023-02-05 18:54:21 +0900

    [1.로그인 처리 : 쿠키, 세션] 프로젝트 세팅

---
+++ login/src/main/java/hello/login/ItemServiceApplication.java
@@ -0,0 +1,17 @@
+package hello.login;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@Slf4j
+@SpringBootApplication
+public class ItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceApplication.class, args);
+		log.info("✅ server started...");
+	}
+
+}
