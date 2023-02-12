ae1c0f2ae9b7411e75a84311e3b38544b658a356
김진영C <jin0kim326@douzone.com>
2023-02-13 08:40:35 +0900

    [API 예외 처리] 시작

---
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -0,0 +1,29 @@
+package hello.exception.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ApiExceptionController {
+
+    @GetMapping("/api/member/{id}")
+    public MemberDto exceptionTest(
+            @PathVariable("id") String id
+    ) {
+        if (id.equals("ex")) {
+            throw new RuntimeException("예외 발생");
+        }
+        return new MemberDto("spring", "jinyoung");
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String memberId;
+        private String name;
+    }
+}
