8e038255c1324cfe16d75c7efb8667f20f82397f
김진영C <jin0kim326@douzone.com>
2023-02-15 13:04:36 +0900

    [API 예외 처리] #5 스프링이 제공하는 ExceptionResolver 1

--- exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,12 +1,15 @@
 package hello.exception.api;
 
+import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class ApiExceptionController {
@@ -28,6 +31,19 @@ public class ApiExceptionController {
         }
 
         return new MemberDto("spring", "jinyoung");
+    }
+
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
+    }
+
+    @GetMapping("/api/response-status-ex2")
+    public String responseStatusEx2() {
+        /**
+         * 개발자가 직접 변경할 수 없는 예외에는 적용불가한 문제를 아래와 같이 해결할 수 있음
+         */
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
     }
 
     @Data
