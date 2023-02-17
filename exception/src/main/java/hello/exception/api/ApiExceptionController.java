4f7de4bdccd331006946c4490693c2f5a32b2771
김진영C <jin0kim326@douzone.com>
2023-02-17 12:57:36 +0900

    [API 예외 처리] #5 스프링이 제공하는 ExceptionResolver 2

--- exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -9,9 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-@Controller
+@RestController
 public class ApiExceptionController {
 
     @GetMapping("/api/member/{id}")
@@ -44,6 +46,11 @@ public class ApiExceptionController {
          * 개발자가 직접 변경할 수 없는 예외에는 적용불가한 문제를 아래와 같이 해결할 수 있음
          */
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+    }
+
+    @GetMapping("/api/default-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+        return "ok";
     }
 
     @Data
