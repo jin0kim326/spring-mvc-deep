6976197b610e4f83c71b9ff13b4b55ac36eab1f7
김진영C <jin0kim326@douzone.com>
2023-02-20 08:46:40 +0900

    [타입컨버터] #1 타입 컨버터 소개

---
+++ typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,44 @@
+package hello.typeconverter.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    /**
+     * ⭐️V1
+     * HTTP 쿼리 스트링으로 전달하는 data는 숫자가 아니라 문자
+     * 형변환을 개발자가 해준 버전
+     */
+    @GetMapping("hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data");
+        Integer intValue = Integer.valueOf(data);
+        System.out.println("intValue = " + intValue);
+
+        return "ok";
+    }
+
+    /**
+     * ⭐️V2
+     * @RequestParam을 이용해서 형변환
+     * 스프링이 내부에서 형변환을 해준다.
+     *
+     * 스프링의 타입 변환 적용 예
+     * 1. 요청 파라미터 (@RequestParam, @ModelAttribute, @PathVariable)
+     * 2. @Value 등으로 YML 정보 읽기
+     * 3. XML에 넣은 스프링 빈 정보 변환
+     * 4. 뷰를 랜더링 할때
+     *
+     * 그 외 새로운 타입을 만들어서 변환하고 싶을때에는 컨버터 인터페이스를 구현해서 등록만 하면됨
+     * 
+     */
+    @GetMapping("hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+}
